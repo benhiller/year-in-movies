8849af3c5cf1060bd70d0620c6f8edb19c9c560b
bhiller <benhiller@gmail.com>
2020-12-03 23:19:40 -0800

    fix warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
     semi: ['error', 'always'],
     'comma-dangle': ['error', 'always-multiline'],
     'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
   plugins: ['react'],
   globals: {
