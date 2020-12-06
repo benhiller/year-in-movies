619746a1ef942113f181187e0984734b8d40f327
bhiller <benhiller@gmail.com>
2020-12-05 20:18:44 -0800

    remove now unnecessary code from page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,12 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import movieData from '../../data/movies.json';
 
 export const getStaticProps = async (context) => {
-  const resp = await fetch(
-    `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Movies?maxRecords=3&view=All%20Entries`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
-      },
-    },
-  );
-  const records = await resp.json();
-
-  const movie = records.records[0];
-  const searchResultsResp = await fetch(
-    // TODO - url encode name
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${movie.fields.Name}`,
-  );
-  const searchResults = await searchResultsResp.json();
-  const matchedSearchResult = searchResults.results.find((searchResult) =>
-    searchResult.release_date.startsWith(movie.fields['Release Year']),
-  );
-
-  return { props: { matchedSearchResult } };
+  return { props: { title: movieData[0].details.title } };
 };
 
-const Home = ({ matchedSearchResult }) => {
+const Home = ({ title }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -33,9 +14,7 @@ const Home = ({ matchedSearchResult }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        Last movie watched: {matchedSearchResult.title}
-      </main>
+      <main className={styles.main}>First movie watched: {title}</main>
     </div>
   );
 };
