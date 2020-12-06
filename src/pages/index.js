f769f45959248ce870069e23b96a219bf2897547
bhiller <benhiller@gmail.com>
2020-12-05 20:47:18 -0800

    add code to determine top directors + actors

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,41 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import movieData from '../../data/movies.json';
 
-export const getStaticProps = async (context) => {
-  return { props: { title: movieData[0].details.title } };
+export const getStaticProps = async () => {
+  const directors = Object.entries(
+    movieData.reduce((acc, movie) => {
+      if (!movie.details.credits) {
+        return acc;
+      }
+      const director = movie.details.credits.crew.find(
+        (crewMember) => crewMember.job === 'Director',
+      );
+      if (acc[director.name]) {
+        acc[director.name] = acc[director.name] + 1;
+      } else {
+        acc[director.name] = 1;
+      }
+      return acc;
+    }, {}),
+  ).sort((a, b) => b[1] - a[1]);
+
+  const actors = Object.entries(
+    movieData.reduce((acc, movie) => {
+      if (!movie.details.credits) {
+        return acc;
+      }
+      movie.details.credits.cast.forEach((castMember) => {
+        if (acc[castMember.name]) {
+          acc[castMember.name] = acc[castMember.name] + 1;
+        } else {
+          acc[castMember.name] = 1;
+        }
+      });
+      return acc;
+    }, {}),
+  ).sort((a, b) => b[1] - a[1]);
+
+  return { props: { title: movieData[0].details.title, directors, actors } };
 };
 
 const Home = ({ title }) => {
