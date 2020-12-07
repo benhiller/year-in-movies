2269233b4b2d6da232e94ac61880294b7475fdd4
bhiller <benhiller@gmail.com>
2020-12-06 18:48:07 -0800

    generate pared down json file for react, update react to use that new file

---
+++ scripts/process_data.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+const readGeneratedData = async () => {
+  const data = await fs.promises.readFile('data/movies.json');
+
+  return JSON.parse(data);
+};
+
+const findDirector = (movie) => {
+  if (!movie.details.credits) {
+    return null;
+  }
+
+  return movie.details.credits.crew.find(
+    (crewMember) => crewMember.job === 'Director',
+  ).name;
+};
+
+const listCastMembers = (movie) => {
+  if (!movie.details.credits) {
+    return null;
+  }
+
+  return movie.details.credits.cast
+    .map((castMember) => {
+      if (castMember.character.includes('uncredited')) {
+        return null;
+      }
+
+      return castMember.name;
+    })
+    .filter((castMember) => !!castMember);
+};
+
+const processData = (data) => {
+  return data.map((movie) => {
+    const processedMovie = {
+      title: movie.details.title,
+      poster: movie.record.fields.Poster[0].url,
+      releaseDate: movie.details.release_date,
+      director: findDirector(movie),
+      cast: listCastMembers(movie),
+      watchDate: movie.record.fields.Date,
+      source: movie.record.fields.Source,
+      notes: movie.record.fields.Notes,
+    };
+
+    return processedMovie;
+  });
+};
+
+readGeneratedData()
+  .then((data) => {
+    return fs.promises.writeFile(
+      'data/processed-movies.json',
+      JSON.stringify(processData(data)),
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,100 @@
+import { useMemo } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import movieData from '../../data/movies.json';
+import movieData from '../../data/processed-movies.json';
 
 export const getStaticProps = async () => {
-  const directors = Object.entries(
-    movieData.reduce((acc, movie) => {
-      if (!movie.details.credits) {
-        return acc;
-      }
-      const director = movie.details.credits.crew.find(
-        (crewMember) => crewMember.job === 'Director',
-      );
-      if (acc[director.name]) {
-        acc[director.name] = acc[director.name] + 1;
-      } else {
-        acc[director.name] = 1;
-      }
-      return acc;
-    }, {}),
-  ).sort((a, b) => b[1] - a[1]);
-
-  const actors = Object.entries(
-    movieData.reduce((acc, movie) => {
-      if (!movie.details.credits) {
-        return acc;
-      }
-      movie.details.credits.cast.forEach((castMember) => {
-        if (acc[castMember.name]) {
-          acc[castMember.name] = acc[castMember.name] + 1;
-        } else {
-          acc[castMember.name] = 1;
-        }
-      });
-      return acc;
-    }, {}),
-  ).sort((a, b) => b[1] - a[1]);
-
-  return { props: { title: movieData[0].details.title, directors, actors } };
+  return {
+    props: {
+      movieData,
+    },
+  };
 };
 
-const Home = ({ title }) => {
+const Home = ({ movieData }) => {
+  const topDirectors = useMemo(
+    () =>
+      Object.entries(
+        movieData.reduce((acc, movie) => {
+          const { director } = movie;
+          if (!director) {
+            return acc;
+          }
+
+          if (acc[director]) {
+            acc[director] = acc[director] + 1;
+          } else {
+            acc[director] = 1;
+          }
+          return acc;
+        }, {}),
+      ).sort((a, b) => b[1] - a[1]),
+    [movieData],
+  );
+
+  const topActors = useMemo(
+    () =>
+      Object.entries(
+        movieData.reduce((acc, movie) => {
+          const { cast } = movie;
+          if (!cast) {
+            return acc;
+          }
+
+          cast.forEach((castMember) => {
+            if (acc[castMember]) {
+              acc[castMember] = acc[castMember] + 1;
+            } else {
+              acc[castMember] = 1;
+            }
+          });
+          return acc;
+        }, {}),
+      ).sort((a, b) => b[1] - a[1]),
+    [movieData],
+  );
+
+  const firstTitle = movieData[0].title;
+  const lastTitle = movieData[movieData.length - 1].title;
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>2020 - Ben&apos;s Year in Movies</title>
+        <title>Ben&apos;s Year in Movies - 2020</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>First movie watched: {title}</main>
+      <main className={styles.main}>
+        <div>First movie watched: {firstTitle}</div>
+        <div>Last movie watched: {lastTitle}</div>
+        <div>
+          Top Directors:
+          <ol>
+            {topDirectors.slice(0, 5).map((director, idx) => (
+              <li key={idx}>{director[0]}</li>
+            ))}
+          </ol>
+        </div>
+        <div>
+          Top Actors:
+          <ol>
+            {topActors.slice(0, 5).map((actor, idx) => (
+              <li key={idx}>{actor[0]}</li>
+            ))}
+          </ol>
+        </div>
+        <div>
+          Posters
+          <br />
+          {movieData.map((movie, idx) => (
+            <img
+              src={movie.poster}
+              key={idx}
+              style={{ width: '50px', height: '75px' }}
+            />
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
