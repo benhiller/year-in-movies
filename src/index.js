3fad822562e3d49b4aa4f096d712d481c1ccd899
bhiller <benhiller@gmail.com>
2020-12-13 20:02:14 -0800

    revert react-snap. not a fan of it resulting in posters shifting after the initial load

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import 'styles/globals.css';
 import App from 'components/App';
 import movieData from 'data/processed-movies.json';
 
-const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <React.StrictMode>
-      <App movieData={movieData} />
-    </React.StrictMode>,
-    rootElement,
-  );
-} else {
-  render(
-    <React.StrictMode>
-      <App movieData={movieData} />
-    </React.StrictMode>,
-    rootElement,
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App movieData={movieData} />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
