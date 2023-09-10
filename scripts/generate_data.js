ecab7e3682a5af0f08dcfce1475761061fc02c05
bhiller <benhiller@gmail.com>
2023-09-10 12:07:10 -0700

    debug

--- scripts/generate_data.js
+++ scripts/generate_data.js
@@ -19,7 +19,7 @@ const fetchAirtablePage = async (baseId, offset) => {
 const fetchAllAirtableRecords = async () => {
   const baseIds = [
     process.env.AIRTABLE_BASE_ID,
-    process.env.AIRTABLE_BASE_ID_2,
+    // process.env.AIRTABLE_BASE_ID_2,
   ];
   let records = [];
   for (const idx in baseIds) {
